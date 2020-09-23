1399bb18fec7215cdb1c6d099307ed4a957a5ff4
Griffin Chure <gchure@caltech.edu>
2020-09-23 10:33:00 -0700

    Updated json specs

--- data/agriculture/FAOSTAT_livestock_product_produced/viz/generate.py
+++ data/agriculture/FAOSTAT_livestock_product_produced/viz/generate.py
@@ -9,19 +9,20 @@ data = pd.read_csv('../processed/FAOSTAT_livestock_and_product.csv')
 data['year'] = pd.to_datetime(data['year'], format='%Y')
 for g, d in data.groupby('subcategory'):
     chart= alt.Chart(d).encode(
-                        x=alt.X(field="year", type="temporal", timeUnit='year'),
+                        x=alt.X(field="year", type="temporal", timeUnit='year',
+                                title="year"),
                         y=alt.Y(field="mass_produced_Mt", 
                                 type="quantitative",
                                 title="produced mass [Mt]"),
                        tooltip=[alt.Tooltip("year:T", timeUnit="year", title="year"),
                                 alt.Tooltip("mass_produced_Mt:Q", format="0.0f", title="mass [Mt]")]
-                      ).properties(width=300, 
+                      ).properties(width="container", 
                                     height=300
                       ).mark_line(color='dodgerblue')
     l = chart.mark_line(color='dodgerblue')
     p = chart.mark_point(filled=True, color='dodgerblue')
-    figure = alt.Layer(l, p)
-    figure.save(f'../vis/{g}.json')
+    figure = alt.layer(l, p)
+    figure.save(f'{g}.json')
 # %%
 
 chart
