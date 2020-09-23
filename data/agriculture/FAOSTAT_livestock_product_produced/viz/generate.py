f209af5de40a9fbc2a7122be2bd2f43c21715e9f
Griffin Chure <gchure@caltech.edu>
2020-09-23 10:38:23 -0700

    Added coarse category spec

--- data/agriculture/FAOSTAT_livestock_product_produced/viz/generate.py
+++ data/agriculture/FAOSTAT_livestock_product_produced/viz/generate.py
@@ -7,6 +7,8 @@ import altair as alt
 # Load the produciton data. 
 data = pd.read_csv('../processed/FAOSTAT_livestock_and_product.csv')
 data['year'] = pd.to_datetime(data['year'], format='%Y')
+
+# Generate JSON vis for subcategories
 for g, d in data.groupby('subcategory'):
     chart= alt.Chart(d).encode(
                         x=alt.X(field="year", type="temporal", timeUnit='year',
@@ -15,7 +17,7 @@ for g, d in data.groupby('subcategory'):
                                 type="quantitative",
                                 title="produced mass [Mt]"),
                        tooltip=[alt.Tooltip("year:T", timeUnit="year", title="year"),
-                                alt.Tooltip("mass_produced_Mt:Q", format="0.0f", title="mass [Mt]")]
+                                alt.Tooltip("mass_produced_Mt:Q", format="0.0f", title="produced mass [Mt]")]
                       ).properties(width="container", 
                                     height=300
                       ).mark_line(color='dodgerblue')
@@ -23,7 +25,28 @@ for g, d in data.groupby('subcategory'):
     p = chart.mark_point(filled=True, color='dodgerblue')
     figure = alt.layer(l, p)
     figure.save(f'{g}.json')
-# %%
 
-chart
+
+# %%
+# Generate JSON vis for grand categories
+# Generate JSON vis for subcategories
+for g, d in data.groupby('category'):
+    chart= alt.Chart(d).encode(
+                        x=alt.X(field="year", type="temporal", timeUnit='year',
+                                title="year"),
+                        y=alt.Y(field="mass_produced_Mt", 
+                                type="quantitative",
+                                title="produced mass [Mt]"),
+                       tooltip=[alt.Tooltip("year:T", timeUnit="year", title="year"),
+                                alt.Tooltip("mass_produced_Mt:Q", format="0.0f", title="produced mass [Mt]")]
+                      ).properties(width="container", 
+                                    height=300
+                      ).mark_line(color='dodgerblue')
+    l = chart.mark_line(color='dodgerblue')
+    p = chart.mark_point(filled=True, color='dodgerblue')
+    figure = alt.layer(l, p)
+    figure.save(f'{g}.json')
+
+
+
 # %%
