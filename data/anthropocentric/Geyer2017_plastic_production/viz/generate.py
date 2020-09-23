aa4a4d1ca4f24ee96c0cab4c57e7b72db0691b53
Griffin Chure <gchure@caltech.edu>
2020-09-23 13:11:46 -0700

    Added BP 2019 power consumption data

--- data/anthropocentric/Geyer2017_plastic_production/viz/generate.py
+++ data/anthropocentric/Geyer2017_plastic_production/viz/generate.py
@@ -9,8 +9,22 @@ data = pd.read_csv('../processed/Geyer2017_plastic_production.csv')
 data['year'] = pd.to_datetime(data['year'], format='%Y')
 
 chart = alt.Chart(data).encode(
-            x=alt.X(field='year', timeUnit='year', title='year'),
+            x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
             y=alt.Y(field='resin_production_Mt', type='quantitative',
-                    title='')
-)
+                    title='plastic resin produced [Mt]'),
+            tooltip=[alt.Tooltip(field='year', type='temporal', timeUnit='year', title='year'),
+                     alt.Tooltip(field='resin_production_Mt', type='quantitative',
+                                 title='produced mass [Mt]')]
+            ).properties(
+            width="container",
+            height=300
+            )
+
+l = chart.mark_line(color='dodgerblue')
+p = chart.mark_point(color='dodgerblue', filled=True)
+
+layer = alt.layer(l, p)
+layer.save('platic_production.json')
+# %%
+
 # %%
---
+++ data/energy/BP_statistical_report_global_energy_usage/viz/generate.py
@@ -0,0 +1,28 @@
+#%%
+import numpy as np
+import pandas as pd 
+import altair as alt
+import anthro.io
+
+# Load the data 
+data = pd.read_csv('../processed/BP_global_energy_usage_by_type.csv')
+data['year'] = pd.to_datetime(data['year'], format='%Y')
+data['consumption_GW'] = data['consumption_TW'].values * 1E3
+data['consumption'] = anthro.io.numeric_formatter(data['consumption_TW'] * 1E12, sci=True, unit='W')
+for g, d in data.groupby(['type']):
+    chart = alt.Chart(d).encode(
+            x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
+            y=alt.Y(field='consumption_GW',  type='quantitative', title='power consumption [GW]'),
+            tooltip=[alt.Tooltip(field='year', type='temporal', title='year', format='%Y'),
+                    alt.Tooltip(field='consumption', type='nominal')]
+            ).properties(
+            width=300,
+            height=300
+            )
+
+    l = chart.mark_line(color='dodgerblue')
+    p = chart.mark_point(color='dodgerblue', filled=True)
+
+    layer = alt.layer(l, p)
+layer
+# %%
