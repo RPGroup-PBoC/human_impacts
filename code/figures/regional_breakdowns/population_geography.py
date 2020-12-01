b4d280f94b8f011c2dd380bf8894a50ffabfb88d
Griffin Chure <gchure@caltech.edu>
2020-11-30 16:09:30 -0800

    Made figures for regional distributions

---
+++ code/figures/regional_breakdowns/population_geography.py
@@ -0,0 +1,63 @@
+#%%
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import anthro.viz
+colors = anthro.viz.plotting_style()
+region_colors, positions = anthro.viz.region_colors(positions=True)
+
+# Load the total, rural, and urban populations
+cats = pd.read_csv('../../../data/anthropocentric/FAOSTAT_world_population/processed/FAOSTAT_rural_urban_population_by_region.csv')
+totals = pd.read_csv('../../../data/anthropocentric/FAOSTAT_world_population/processed/FAOSTAT_population_by_region.csv')
+
+# Restrict to the 2018 values for both datasets.
+cats = cats[cats['year']==2018]
+totals = totals[totals['year']==2018]
+
+# Add colors to each regions
+cats['color'] = [region_colors[k] for k in cats['region'].values]
+totals['color'] = [region_colors[k] for k in totals['region'].values]
+
+# Compute the precentage breakdown for each.
+total_pop = totals['population_Mhd'].sum()
+totals['fraction'] = totals['population_Mhd'].values / total_pop
+
+dfs = []
+for g, d in cats.groupby(['category']):
+    d = d.copy()
+    tot = d['population_Mhd'].sum()
+    d['fraction'] = d['population_Mhd'].values / tot
+    dfs.append(d)
+cats = pd.concat(dfs)
+
+# %% Total donut
+circ = plt.Circle((0, 0), 0.6, color='white')
+fig, ax = plt.subplots(1,1,  figsize=(2.5, 2.5))
+totals.sort_values('population_Mhd', inplace=True)
+ax.pie(totals['population_Mhd'], colors=totals['color'])
+ax.add_artist(circ)
+plt.savefig('../../../figures/regional_breakdowns/total_population_donut.svg')
+
+
+# %% Rural donut
+circ = plt.Circle((0, 0), 0.6, color='white')
+fig, ax = plt.subplots(1,1,  figsize=(2.5, 2.5))
+rural = cats[cats['category']=='rural']
+rural.sort_values('population_Mhd', inplace=True)
+ax.pie(rural['population_Mhd'], colors=rural['color'])
+ax.add_artist(circ)
+plt.savefig('../../../figures/regional_breakdowns/rural_population_donut.svg')
+
+
+# %% Urban donut
+circ = plt.Circle((0, 0), 0.6, color='white')
+fig, ax = plt.subplots(1,1,  figsize=(2.5, 2.5))
+urban = cats[cats['category']=='urban']
+rural.sort_values('population_Mhd', inplace=True)
+ax.pie(urban['population_Mhd'], colors=urban['color'])
+ax.add_artist(circ)
+plt.savefig('../../../figures/regional_breakdowns/urban_population_donut.svg')
+
+
+
+# %%
--- data/anthropocentric/FAOSTAT_world_population/process_by_region.py
+++ data/anthropocentric/FAOSTAT_world_population/process_by_region.py
@@ -1,12 +1,19 @@
 #%%
 import numpy as np 
 import pandas as pd 
-data = pd.read_csv('./source/FAOSTAT_population_by_region_reduced.csv')
+data = [pd.read_csv('./source/FAOSTAT_population_by_region_reduced.csv'),
+        pd.read_csv('./source/FAOSTAT_rural_urban_population_by_region_reduced.csv')]
 
 
 # Aggregate 'Caribbean' into 'Northern America'
-data.loc[data['region']=='Caribbean', 'region'] = 'Northern America'
-grouped = data.groupby(['region', 'year']).sum().reset_index()
-grouped.to_csv('./processed/FAOSTAT_population_by_region.csv', index=False)
+fnames = ['FAOSTAT_population_by_region.csv', 'FAOSTAT_rural_urban_population_by_region.csv']
+for i, d in enumerate(data):
+    d.loc[d['region']=='Caribbean', 'region'] = 'Northern America'
+    if i == 0:
+        groupby = ['region', 'year']
+    elif i == 1:
+        groupby = ['region', 'category', 'year']
+    grouped = d.groupby(groupby).sum().reset_index()
+    grouped.to_csv(f'./processed/{fnames[i]}', index=False)
 
 # %%
