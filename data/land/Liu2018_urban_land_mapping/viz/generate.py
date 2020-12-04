f877134cb582d1c5b0cf5841d288f257ffa56dd6
Griffin Chure <gchure@caltech.edu>
2020-12-03 18:43:11 -0800

    Finally adding a data record for the Liu 2018 urban land area dataset

---
+++ data/land/Liu2018_urban_land_mapping/viz/generate.py
@@ -0,0 +1,25 @@
+#%%
+import pandas as pd 
+import altair as alt
+import anthro.io
+
+# Load the data sets 
+data = pd.read_csv('../processed/Liu2018_table_3.csv')
+data['year'] = pd.to_datetime(data['year'], format='%Y')
+total = data[data['region']=='total']
+
+#%%
+chart = alt.Chart(total).encode(
+        x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
+        y=alt.Y(field='area_km2', type='quantitative', title='land area [billion hectares]',
+                 scale=alt.Scale(domain=[4, 5.25])),
+        tooltip=[alt.Tooltip(field='year', type='temporal', format='%Y', title='year'),
+                 alt.Tooltip(field='area_km2', type='nominal', title='land area [sq. km]')]
+        ).properties(width='container', height=300)
+l = chart.mark_line(color='dodgerblue')
+p = chart.mark_point(color='dodgerblue', filled=True)
+layer = alt.layer(l, p)
+
+layer.save('./urban_land_area.json')
+
+# %%
