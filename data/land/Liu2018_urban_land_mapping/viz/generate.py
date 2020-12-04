fcb6cbfaeea081e21d1a796af018853fe324baa3
Griffin Chure <gchure@caltech.edu>
2020-12-03 18:51:53 -0800

    Fixed a problem with the viz for the Liu data

--- data/land/Liu2018_urban_land_mapping/viz/generate.py
+++ data/land/Liu2018_urban_land_mapping/viz/generate.py
@@ -6,12 +6,12 @@ import anthro.io
 # Load the data sets 
 data = pd.read_csv('../processed/Liu2018_table_3.csv')
 data['year'] = pd.to_datetime(data['year'], format='%Y')
-total = data[data['region']=='total']
+total = data[data['region']=='Total']
 
 #%%
 chart = alt.Chart(total).encode(
         x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
-        y=alt.Y(field='area_km2', type='quantitative', title='land area [billion hectares]',
+        y=alt.Y(field='area_km2', type='quantitative', title='land area [sq. km]',
                  scale=alt.Scale(domain=[4, 5.25])),
         tooltip=[alt.Tooltip(field='year', type='temporal', format='%Y', title='year'),
                  alt.Tooltip(field='area_km2', type='nominal', title='land area [sq. km]')]
@@ -21,5 +21,7 @@ p = chart.mark_point(color='dodgerblue', filled=True)
 layer = alt.layer(l, p)
 
 layer.save('./urban_land_area.json')
+
+
 
 # %%
