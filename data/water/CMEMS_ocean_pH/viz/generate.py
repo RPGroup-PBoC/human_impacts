c2d8903da0b1e98a9128972b3f93346bf9c56cad
ilopezgp <ilopezgp@gmail.com>
2020-12-04 20:19:15 -0800

    Add json plot for H percentage icncrease.

--- data/water/CMEMS_ocean_pH/viz/generate.py
+++ data/water/CMEMS_ocean_pH/viz/generate.py
@@ -24,3 +24,24 @@ layer = alt.layer(l, p)
 layer.save('surface_ocean_pH.json')
 
 # %%
+
+# Load the production data. 
+data = pd.read_csv('../processed/CMEMS_trends_ocean_pH.csv')
+data['year'] = pd.to_datetime(data['year'], format='%Y')
+data['[H+] percentage trend'] = data[data['Measure type']=='[H+] percentage trend']['Value']
+#%%
+
+# Generate a plot for global average surface pH
+chart = alt.Chart(data).encode(
+            x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
+            y=alt.Y(field='[H+] percentage trend', type='quantitative', title='[H+] % change'),
+            tooltip=[alt.Tooltip(field='year', type='temporal', title='year', format='%Y'),
+                     alt.Tooltip(field='[H+] percentage trend', type='nominal', title='[H+] % change')]
+            ).properties(width='container', height=300)
+
+l = chart.mark_line(color='dodgerblue')
+p = chart.mark_point(color='dodgerblue', filled=True)
+layer = alt.layer(l, p)
+layer.save('percentage_change_H.json')
+
+# %%
