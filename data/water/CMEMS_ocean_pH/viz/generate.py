5e47ef03f2c91cfaf37dc769228e28e131631725
ilopezgp <ilopezgp@gmail.com>
2020-12-04 19:22:03 -0800

    Create plot for ocean pH evolution.

---
+++ data/water/CMEMS_ocean_pH/viz/generate.py
@@ -0,0 +1,26 @@
+#%%
+import numpy as np 
+import pandas as pd 
+import altair as alt 
+import anthro.io
+
+# Load the production data. 
+data = pd.read_csv('../processed/CMEMS_average_ocean_pH.csv')
+data['year'] = pd.to_datetime(data['year'], format='%Y')
+data['pH'] = anthro.io.numeric_formatter(data['pH'].values, sci=False)
+#%%
+
+# Generate a plot for global average surface pH
+chart = alt.Chart(data).encode(
+            x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
+            y=alt.Y(field='pH', type='quantitative', title='ocean pH'),
+            tooltip=[alt.Tooltip(field='year', type='temporal', title='year', format='%Y'),
+                     alt.Tooltip(field='pH', type='nominal', title='pH')]
+            ).properties(width='container', height=300)
+
+l = chart.mark_line(color='dodgerblue')
+p = chart.mark_point(color='dodgerblue', filled=True)
+layer = alt.layer(l, p)
+layer.save('surface_ocean_pH.json')
+
+# %%
