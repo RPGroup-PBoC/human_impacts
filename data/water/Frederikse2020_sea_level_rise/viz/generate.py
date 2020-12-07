5773ff0b8a9a9bc56ff86c46efbb05417b2e077e
Griffin Chure <gchure@caltech.edu>
2020-12-07 10:42:28 -0800

    Regenerated figures with appropriate sizing

--- data/water/Frederikse2020_sea_level_rise/viz/generate.py
+++ data/water/Frederikse2020_sea_level_rise/viz/generate.py
@@ -41,10 +41,11 @@ for g, d in contrib.groupby('source'):
         ).properties(width='container', height=300)
     a = chart.mark_area(color='dodgerblue', fillOpacity=0.4).encode(
         x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
-        y='observed_GMSL_lower:Q',
-        y2='observed_GMSL_upper:Q')
+        y='lower:Q',
+        y2='upper:Q')
     l = chart.mark_line(color='dodgerblue')
     p = chart.mark_point(color='dodgerblue', filled=True)
 
     layer = alt.layer(a, l, p)
     layer.save(f"{'_'.join(g.lower().split(' '))}_GMSL.json")
+# %%
