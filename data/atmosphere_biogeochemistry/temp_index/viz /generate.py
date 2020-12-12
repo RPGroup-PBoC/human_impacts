3bb709feef6a87d2d3f81cdceee07deb1fb731f1
nsarai <nsarai@caltech.edu>
2020-12-11 17:30:06 -0800

    Added surface temp viz

---
+++ data/atmosphere_biogeochemistry/temp_index/viz /generate.py
@@ -0,0 +1,24 @@
+#%%
+import pandas as pd 
+import altair as alt
+import anthro.io
+
+# Load the data sets 
+data = pd.read_csv('../processed/Land-OceanTemperatureIndex.csv')
+data['year'] = pd.to_datetime(data['Year'], format='%Y')
+data = data.groupby(['year']).sum()['°C'].reset_index()
+
+#%%
+chart = alt.Chart(data).encode(
+        x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
+        y=alt.Y(field='°C', type='quantitative', title='°C'),
+        tooltip=[alt.Tooltip(field='year', type='temporal', format='%Y', title='year'),
+                 alt.Tooltip(field='°C', type='nominal', title='°C')]
+        ).properties(width='container', height=300)
+l = chart.mark_line(color='dodgerblue')
+p = chart.mark_point(color='dodgerblue', filled=True)
+layer = alt.layer(l, p)
+    
+layer.save('./surface_temp.json')
+# %%
+
