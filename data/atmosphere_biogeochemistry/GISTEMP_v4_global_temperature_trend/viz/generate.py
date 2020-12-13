e5cb464141dd051c04b1eff5c10ac9add7d5deeb
nsarai <nsarai@caltech.edu>
2020-12-12 18:15:36 -0800

    Revissions to GISTEMP global surface temp

--- data/atmosphere_biogeochemistry/GISTEMP_v4_global_temperature_trend/viz/generate.py
+++ data/atmosphere_biogeochemistry/GISTEMP_v4_global_temperature_trend/viz/generate.py
@@ -1,9 +1,9 @@
 #%%
-import pandas as pd 
+import pandas as pd
 import altair as alt
 import anthro.io
 
-# Load the data sets 
+# Load the data sets
 data = pd.read_csv('../processed/Land-OceanTemperatureIndex.csv')
 data['year'] = pd.to_datetime(data['Year'], format='%Y')
 data = data.groupby(['year']).sum()['°C'].reset_index()
@@ -15,10 +15,13 @@ chart = alt.Chart(data).encode(
         tooltip=[alt.Tooltip(field='year', type='temporal', format='%Y', title='year'),
                  alt.Tooltip(field='°C', type='nominal', title='°C')]
         ).properties(width='container', height=300)
+a = chart.mark_area(color='dodgerblue', fillOpacity=0.4).encode(
+    x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
+    y='°C_lower:Q',
+    y2='°C_upper:Q').properties(width='container', height=300)
 l = chart.mark_line(color='dodgerblue')
 p = chart.mark_point(color='dodgerblue', filled=True)
-layer = alt.layer(l, p)
-    
+layer = alt.layer(a, l, p)
+
 layer.save('./surface_temp.json')
 # %%
-
