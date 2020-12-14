4ad17b5cc62f74bccf4eef129ca820e1261e5126
Griffin Chure <gchure@caltech.edu>
2020-12-14 10:38:35 -0800

    Update and addition of new region definitions

--- data/agriculture/FAOSTAT_livestock_population/process_continent_data.py
+++ data/agriculture/FAOSTAT_livestock_population/process_continent_data.py
@@ -1,14 +1,16 @@
 #%%
 import numpy as np 
 import pandas as pd 
+import anthro.io
+mapper = anthro.io.region_mapper()
 data = pd.read_csv('./source/FAOSTAT_livestock_population_continent_reduced.csv')
-data.head()
 
+# Remap the regions
+data['region'] = [mapper[k] for k in data['region'].values] 
 data['population_Mhd'] = data['value'].values / 1E6
 data.loc[data['unit'].str.contains('1000'), 'population_Mhd'] *= 1000
 data.drop(columns=['unit', 'value'], inplace=True)
 data = data[data['animal']!='Beehives']
 data['animal'] = np.array([s.lower() for s in data['animal'].values])
-data.loc[data['region']=='Caribbean', 'region'] = 'Northern America'
 data.to_csv('./processed/FAOSTAT_livestock_population_continent.csv', index=False)
 # %%
