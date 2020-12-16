32358b4e3c309b0489f4b89feced78317b1c9a7a
nsarai <nsarai@caltech.edu>
2020-12-16 12:53:15 -0800

    Update IFASTAT nitrogen viz units

--- data/atmosphere_biogeochemistry/IFASTAT_nitrogen_production/viz/generate.py
+++ data/atmosphere_biogeochemistry/IFASTAT_nitrogen_production/viz/generate.py
@@ -7,14 +7,14 @@ import altair as alt
 data = pd.read_csv('../processed/IFA_ammonia_tidy.csv')
 proc_data = pd.DataFrame()
 proc_data['year'] = pd.to_datetime(data['year'], format='%Y')
-proc_data['production (kg)'] = data['value']*1000000
+proc_data['production (kt)'] = data['value']
 
 # Generate a plot for global mean surface temperature
 chart = alt.Chart(proc_data).encode(
             x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='production (kg)', type='quantitative', title='Mass of nitrogen in synthesized ammonia [kg]', scale=alt.Scale(domain=[120000000000, 160000000000])),
+            y=alt.Y(field='production (kt)', type='quantitative', title='Mass of nitrogen in synthesized ammonia [kt]', scale=alt.Scale(domain=[120000, 160000])),
             tooltip=[alt.Tooltip(field='year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='production (kg)', type='quantitative', title='production mass [kg]', format='.6s')]
+                     alt.Tooltip(field='production (kt)', type='quantitative', title='production mass [kt]', format='.1f')]
             ).properties(width='container', height=300)
 
 l = chart.mark_line(color='dodgerblue')
