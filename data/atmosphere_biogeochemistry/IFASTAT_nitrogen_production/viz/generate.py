bac0cafc5bfa6d647f2688e944f8727f9b3a568c
nsarai <nsarai@caltech.edu>
2020-12-16 12:38:40 -0800

    Update IFASTAT nitrogen viz

--- data/atmosphere_biogeochemistry/IFASTAT_nitrogen_production/viz/generate.py
+++ data/atmosphere_biogeochemistry/IFASTAT_nitrogen_production/viz/generate.py
@@ -3,31 +3,22 @@ import numpy as np
 import pandas as pd
 import altair as alt
 
-# Load the NOAAGlobalTempv5 data.
-data = pd.read_csv('../processed/GISTEMPv4_global_surf_temperature_trend.csv')
+# Load the IFASTAT nitrogen data.
+data = pd.read_csv('../processed/IFA_ammonia_tidy.csv')
 proc_data = pd.DataFrame()
-proc_data['year'] = pd.to_datetime(data[data['Reported value']=='global mean']['Year'], format='%Y')
-proc_data['global mean'] = data[data['Reported value']=='global mean']['Temperature anomaly (K)']
-proc_data['lower bound'] = data[data['Reported value']=='low. bound, 95% CI bias unc.']['Temperature anomaly (K)'].to_numpy()
-proc_data['upper bound'] = data[data['Reported value']=='upp. bound, 95% CI bias unc.']['Temperature anomaly (K)'].to_numpy()
+proc_data['year'] = pd.to_datetime(data['year'], format='%Y')
+proc_data['production (kg)'] = data['value']*1000000
 
 # Generate a plot for global mean surface temperature
 chart = alt.Chart(proc_data).encode(
             x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='global mean', type='quantitative', title='Global surface temperature change from the 1850-1900 mean [°C]', scale=alt.Scale(domain=[-0.5, 1.4])),
+            y=alt.Y(field='production (kg)', type='quantitative', title='Mass of nitrogen in synthesized ammonia [kg]', scale=alt.Scale(domain=[120000000000, 160000000000])),
             tooltip=[alt.Tooltip(field='year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='global mean', type='quantitative', title='global mean change [°C]', format='0.2f')]
+                     alt.Tooltip(field='production (kg)', type='quantitative', title='production mass [kg]', format='.6s')]
             ).properties(width='container', height=300)
 
-# Add uncertainty bands
-bands = chart.mark_area(color='darkorange', fillOpacity=0.4).encode(
-            x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
-            y='lower bound:Q',
-            y2='upper bound:Q'
-        ).properties(width='container', height=300)
-
-l = chart.mark_line(color='darkorange')
-p = chart.mark_point(color='darkorange', filled=True)
-layer = alt.layer(bands, l, p) #.resolve_scale(y='shared')
-layer.save('surface_temp.json')
+l = chart.mark_line(color='dodgerblue')
+p = chart.mark_point(color='dodgerblue', filled=True)
+layer = alt.layer(l, p)
+layer.save('nitrogen_production.json')
 # %%
