b90ec2fd0045677a297895e079626de9d5feb17e
nsarai <nsarai@caltech.edu>
2021-01-20 22:12:05 -0800

    Update BP extraction data

--- data/anthropocentric/WorldSteelAssoc_steel_production/viz/generate.py
+++ data/anthropocentric/WorldSteelAssoc_steel_production/viz/generate.py
@@ -4,14 +4,14 @@ import pandas as pd
 import altair as alt
 import anthro.io
 
-# Load the IFASTAT nitrogen data.
+# Load the USGS steel data.
 data = pd.read_csv('../processed/WorldSteelAssoc_steel_production_1950-2019.csv')
 proc_data = pd.DataFrame()
 proc_data['year'] = pd.to_datetime(data['year'], format='%Y')
 proc_data['production (kg)'] = data['value']
 proc_data['label'] = anthro.io.numeric_formatter(data['value'].values * 1E3, unit='kg')
 
-# Generate a plot for global nitrogen production.
+# Generate a plot for global steel production.
 chart = alt.Chart(proc_data).encode(
             x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
             y=alt.Y(field='production (kg)', type='quantitative', title='Mass of steel [kg]', scale=alt.Scale(domain=[100000000000, 2000000000000])),
