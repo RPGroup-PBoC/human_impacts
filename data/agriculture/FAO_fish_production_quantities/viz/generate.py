d3409e4c74a77dd46a0cc033e00ac1914397add6
ilopezgp <ilopezgp@gmail.com>
2021-02-08 17:55:27 -0800

    Add fish stocks viz

--- data/agriculture/FAO_fish_production_quantities/viz/generate.py
+++ data/agriculture/FAO_fish_production_quantities/viz/generate.py
@@ -6,17 +6,19 @@ import anthro.io
 # Load the FishStatJ data for total produced fish mass by source 
 data = pd.read_csv('../processed/FAO_FishStatJ_total_mass_source.csv')
 data['year'] = pd.to_datetime(data['year'], format='%Y')
-
+bounds = [[0, 120], [0, 100], [0, 200]]
+i = 0
 for g, d in data.groupby(['source']):
   chart = alt.Chart(d).encode(
               x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
-              y=alt.Y(field='produced mass (Mt)', type='quantitative', title='Harvested mass [Mt]'),
+              y=alt.Y(field='produced mass (Mt)', type='quantitative', title='Harvested mass [Mt]',
+              scale=alt.Scale(domain=bounds[i])),
               tooltip=[alt.Tooltip(field='year', type='temporal', title='year', format='%Y'),
                        alt.Tooltip(field='produced mass (Mt)', type='nominal', title='Harvested mass [Mt]')]
   ).properties(width="container", height=300)
   l = chart.mark_line(color='dodgerblue')
   p = chart.mark_point(color='dodgerblue', filled=True)
-
+  i += 1
   layer = alt.layer(l, p)
   layer.save(f'{g}_fish_mass.json')
 
@@ -35,4 +37,21 @@ p = chart.mark_point(color='dodgerblue', filled=True)
 
 layer = alt.layer(l, p)
 layer.save(f'aquaculture_mass_percentage.json')
+
+# Generate marine fish stock state viz
+stock_data = pd.read_csv('../processed/marine_fish_stock_state.csv')
+stock_data = stock_data[stock_data['Fishery status'] == 'overfished']
+stock_data['year'] = pd.to_datetime(stock_data['year'], format='%Y')
+
+chart = alt.Chart(stock_data).encode(
+            x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
+            y=alt.Y(field='Percentage of fish stocks', type='quantitative', title='Overfished fish stocks [%]',
+            scale=alt.Scale(domain=[5, 40])),
+            tooltip=[alt.Tooltip(field='year', type='temporal', title='year', format='%Y'),
+                     alt.Tooltip(field='Percentage of fish stocks', type='nominal', title='Overfished fish stocks [%]')]
+).properties(width="container", height=300)
+l = chart.mark_line(color='dodgerblue')
+p = chart.mark_point(color='dodgerblue', filled=True)
+layer = alt.layer(l, p)
+layer.save(f'overfished_fish_stocks.json')
 # %%
