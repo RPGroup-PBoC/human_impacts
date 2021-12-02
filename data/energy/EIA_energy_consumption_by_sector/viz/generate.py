b2758245edac0946080a647f1dbfd1098c0f0b27
ilopezgp <ilopezgp@gmail.com>
2021-12-01 17:40:56 -0800

    Viz formatting.

--- data/energy/EIA_energy_consumption_by_sector/viz/generate.py
+++ data/energy/EIA_energy_consumption_by_sector/viz/generate.py
@@ -6,21 +6,22 @@ import anthro.io
 
 # Generate a plot for sectorial energy consumption (TW) from EIA International Outlook
 data = pd.read_csv('../processed/Passenger_transportation_energy_use.csv')
+data['Year'] = pd.to_datetime(data['Year'].astype(str), format='%Y', errors='coerce')
 
 targets = ['Passenger light-duty vehicles',
             'Passenger air transport',
             'Total passenger transport']
-target_domains = [[1.1, 1.7], [0.1, 0.4], [1.4, 2.6]]
+target_domains = [[1.1, 1.7], [0.1, 0.6], [1.4, 2.6]]
 for target, domain in zip(targets, target_domains):
    agg_data = pd.DataFrame()
    agg_data['Year'] = (data[data['Reported sector']==target])['Year']
    agg_data['Energy consumption (TW)'] = (data[data['Reported sector']==target])['Energy consumption (TW)']
 
    chart = alt.Chart(agg_data).encode(
-               x=alt.X(field='Year', type='quantitative', title='Year'),
+               x=alt.X(field='Year', type='temporal', timeUnit='year', title='Year'),
                y=alt.Y(field='Energy consumption (TW)', type='quantitative', title=target+' consumption (TW)',
                    scale=alt.Scale(domain=domain)),
-               tooltip=[alt.Tooltip(field='Year', type='quantitative', title='Year'),
+               tooltip=[alt.Tooltip(field='Year', type='temporal', title='Year', format='%Y'),
                         alt.Tooltip(field=r'Energy consumption (TW)', type='nominal', title=r'Energy consumption (TW)')]
                ).properties(width='container', height=300)
 
--- data/energy/EPA_automotive/viz/generate.py
+++ data/energy/EPA_automotive/viz/generate.py
@@ -6,16 +6,17 @@ import anthro.io
 
 # Generate a plot for fuel economy of all US light-duty vehicles
 data = pd.read_csv('../processed/tidy_automotive_trends.csv')
+data['Model Year'] = pd.to_datetime(data['Model Year'].astype(str), format='%Y', errors='coerce')
 data = data[(data['Reported quantity']=='Real-world fuel economy (MPG)') & (data['Regulatory Class']=='All')]
 agg_data = pd.DataFrame()
 agg_data['Year'] = data['Model Year']
 agg_data['Fuel economy'] = round(data['Reported value']/2.352, 3)
 
 chart = alt.Chart(agg_data).encode(
-            x=alt.X(field='Year', type='quantitative', title='Year'),
+            x=alt.X(field='Year', type='temporal', timeUnit='year', title='Year'),
             y=alt.Y(field=r'Fuel economy', type='quantitative', title=r'Fuel economy (km/L)',
                 scale=alt.Scale(domain=[5, 12])),
-            tooltip=[alt.Tooltip(field='Year', type='quantitative', title='Year'),
+            tooltip=[alt.Tooltip(field='Year', type='temporal', title='Year', format='%Y'),
                      alt.Tooltip(field=r'Fuel economy', type='quantitative', title=r'Fuel economy (km/L)')]
             ).properties(width='container', height=300)
 
@@ -27,16 +28,17 @@ layer.save('fuel_economy_usa.json')
 
 # Generate a plot for CO2 emissions of all US light-duty vehicles
 data = pd.read_csv('../processed/tidy_automotive_trends.csv')
+data['Model Year'] = pd.to_datetime(data['Model Year'].astype(str), format='%Y', errors='coerce')
 data = data[(data['Reported quantity']=='Real-world CO2 emissions (g/mi)') & (data['Regulatory Class']=='All')]
 agg_data = pd.DataFrame()
 agg_data['Year'] = data['Model Year']
 agg_data['CO2 emissions'] = round(data['Reported value'] * 0.621371 / 1000.0, 3)
 
 chart = alt.Chart(agg_data).encode(
-            x=alt.X(field='Year', type='quantitative', title='Year'),
+            x=alt.X(field='Year', type='temporal', timeUnit='year', title='Year'),
             y=alt.Y(field=r'CO2 emissions', type='quantitative', title=r'CO2 emissions (kg/km)',
                 scale=alt.Scale(domain=[0.2, 0.5])),
-            tooltip=[alt.Tooltip(field='Year', type='quantitative', title='Year'),
+            tooltip=[alt.Tooltip(field='Year', type='temporal', title='Year', format='%Y'),
                      alt.Tooltip(field=r'CO2 emissions', type='quantitative', title=r'CO2 emissions (kg/km)')]
             ).properties(width='container', height=300)
 
