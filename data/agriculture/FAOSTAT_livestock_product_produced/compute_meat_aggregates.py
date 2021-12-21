cda53617c9aee8249a58660515467c2ae971964c
gchure <griffinchure@gmail.com>
2021-12-20 23:23:05 -0800

    Fixed chicken slaughter data

--- data/agriculture/FAOSTAT_livestock_product_produced/compute_meat_aggregates.py
+++ data/agriculture/FAOSTAT_livestock_product_produced/compute_meat_aggregates.py
@@ -2,14 +2,10 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('processed/FAOSTAT_aggregated_livestock_product_categorized.csv')
-data = data[~data['product'].str.contains('indigenous')]
-grouped = data.groupby(['product']).sum().reset_index()
-grouped.drop(columns=['year'])
-grouped.to_csv('processed/FAOSTAT_aggregated_livestock_product_cumulative.csv')
+data = pd.read_csv('processed/FAOSTAT_livestock_and_product.csv')
+grouped = data.groupby(['subcategory']).sum().reset_index()
+grouped.drop(columns=['year', 'Unnamed: 0', 'yield_kg_per_head'], inplace=True)
+grouped.to_csv('./processed/FAOSTAT_aggregated_livestock_product_cumulative.csv')
 
-
-# %%
-grouped
 
 # %%
