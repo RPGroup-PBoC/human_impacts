e47b93ec9f44a95100925165a188bbb44fbc2568
nsarai <nsarai@caltech.edu>
2022-02-16 11:31:42 -0800

    Update IFAstat nitrogen viz

--- data/atmosphere_biogeochemistry/IFASTAT_nitrogen_production/viz/generate.py
+++ data/atmosphere_biogeochemistry/IFASTAT_nitrogen_production/viz/generate.py
@@ -8,15 +8,14 @@ import anthro.io
 data = pd.read_csv('../processed/IFA_ammonia_production_2008-2020.csv')
 proc_data = pd.DataFrame()
 proc_data['year'] = pd.to_datetime(data['year'], format='%Y')
-proc_data['production (kg)'] = data['value']
-proc_data['label'] = anthro.io.numeric_formatter(data['value'].values * 1E3, unit='kg')
+proc_data['production'] = data['value']
 
 # Generate a plot for global nitrogen production.
 chart = alt.Chart(proc_data).encode(
             x=alt.X(field='year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='production (kg)', type='quantitative', title='Mass of nitrogen in synthesized ammonia [kg]', scale=alt.Scale(domain=[120000000000, 160000000000])),
+            y=alt.Y(field='production', type='quantitative', title='Nitrogen production (Mt)', scale=alt.Scale(zero=False)),
             tooltip=[alt.Tooltip(field='year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='label', type='nominal', title='production mass')]
+                     alt.Tooltip(field='production', type='nominal', title='Production (Mt)')]
             ).properties(width='container', height=300)
 
 l = chart.mark_line(color='dodgerblue')
