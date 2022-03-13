86f95d0869f20892d0797189822cc7c19d23953d
nsarai <nsarai@caltech.edu>
2022-03-13 16:49:21 -0700

    Add legends to sea ice timecourses

--- data/water/NOAA_sea_ice_index/viz/generate_northern.py
+++ data/water/NOAA_sea_ice_index/viz/generate_northern.py
@@ -7,82 +7,49 @@ import anthro.io
 # Load the Northern Hemisphere sea ice area data
 northern_area_data = pd.read_csv('../processed/northern_hemisphere_area.csv')
 northern_area_data['Year'] = pd.to_datetime(northern_area_data['Year'], format='%Y')
-northern_march_area = northern_area_data[northern_area_data['Month']=='March']
-northern_september_area = northern_area_data[northern_area_data['Month']=='September']
-northern_annual_area = northern_area_data[northern_area_data['Month']=='Annual']
+northern_area_data = northern_area_data.drop(northern_area_data[(northern_area_data.Month != 'Annual') & (northern_area_data.Month != 'March') & (northern_area_data.Month != 'September')].index)
 
-# Generate a plot for March sea ice area
-chart = alt.Chart(northern_march_area).encode(
-            x=alt.X(field='Year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='Area', type='quantitative', title='sea ice area (sq. km)', scale=alt.Scale(zero=False)),
-            tooltip=[alt.Tooltip(field='Year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='Area', type='nominal', title='sea ice area in March (sq. km)')]
-            ).properties(width='container', height=300)
-l1 = chart.mark_line(color='dodgerblue')
-p1 = chart.mark_point(color='dodgerblue', filled=True)
+# Generate a plot for sea ice area in March, September, and Annual
+base = alt.Chart(northern_area_data).encode(alt.X(field='Year', type='temporal', timeUnit='year', title='year')).properties(width='container', height=300)
+columns = sorted(northern_area_data.Month.unique())
+selection = alt.selection_single(
+    fields=['Year'], nearest=True, on='mouseover', empty='none', clear='mouseout'
+)
 
-# Generate a plot for September sea ice area
-chart = alt.Chart(northern_september_area).encode(
-            x=alt.X(field='Year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='Area', type='quantitative', title='sea ice area (sq. km)', scale=alt.Scale(zero=False)),
-            tooltip=[alt.Tooltip(field='Year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='Area', type='nominal', title='sea ice area in September (sq. km)')]
-            ).properties(width='container', height=300)
-l2 = chart.mark_line(color='dodgerblue')
-p2 = chart.mark_point(color='dodgerblue', filled=True)
+lines = base.mark_line(point=True).encode(alt.Y(field='Area', type = 'quantitative', title ='sea ice area (sq. km)'), color=alt.Color('Month:N', scale=alt.Scale(range=['rebeccapurple', 'dodgerblue', 'tomato'])))
+points = lines.mark_point().transform_filter(selection)
 
-# Generate a plot for Annual sea ice area
-chart = alt.Chart(northern_annual_area).encode(
-            x=alt.X(field='Year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='Area', type='quantitative', title='sea ice area (sq. km)', scale=alt.Scale(zero=False)),
-            tooltip=[alt.Tooltip(field='Year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='Area', type='nominal', title='annually averaged sea ice area (sq. km)')]
-            ).properties(width='container', height=300)
-l3 = chart.mark_line(color='dodgerblue')
-p3 = chart.mark_point(color='dodgerblue', filled=True)
+rule = base.transform_pivot(
+    'Month', value='Area', groupby=['Year']
+).mark_rule().encode(
+    opacity=alt.condition(selection, alt.value(0.3), alt.value(0)),
+    tooltip=[alt.Tooltip(field = c, type='quantitative') for c in columns]
+).add_selection(selection)
 
-layer = alt.layer(l1,p1,l2,p2,l3,p3)
-layer.save('./northern_area.json')
+northern_area_layer = alt.layer(lines, points, rule)
+northern_area_layer.save('./northern_area.json')
 
 # Load the Northern Hemisphere sea ice extent data
 northern_extent_data = pd.read_csv('../processed/northern_hemisphere_extent.csv')
 northern_extent_data['Year'] = pd.to_datetime(northern_extent_data['Year'], format='%Y')
-northern_march_extent = northern_extent_data[northern_extent_data['Month']=='March']
-northern_september_extent = northern_extent_data[northern_extent_data['Month']=='September']
-northern_annual_extent = northern_extent_data[northern_extent_data['Month']=='Annual']
+northern_extent_data = northern_extent_data.drop(northern_extent_data[(northern_extent_data.Month != 'Annual') & (northern_extent_data.Month != 'March') & (northern_extent_data.Month != 'September')].index)
 
-# Generate a plot for March sea ice extent
-chart = alt.Chart(northern_march_extent).encode(
-            x=alt.X(field='Year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='Extent', type='quantitative', title='sea ice extent (sq. km)', scale=alt.Scale(zero=False)),
-            tooltip=[alt.Tooltip(field='Year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='Extent', type='nominal', title='sea ice extent in March (sq. km)')]
-            ).properties(width='container', height=300)
+# Generate a plot for sea ice extent in March, September, and Annual
+base = alt.Chart(northern_extent_data).encode(alt.X(field='Year', type='temporal', timeUnit='year', title='year')).properties(width='container', height=300)
+columns = sorted(northern_extent_data.Month.unique())
+selection = alt.selection_single(
+    fields=['Year'], nearest=True, on='mouseover', empty='none', clear='mouseout'
+)
 
-l4 = chart.mark_line(color='dodgerblue')
-p4 = chart.mark_point(color='dodgerblue', filled=True)
+lines = base.mark_line(point=True).encode(alt.Y(field='Extent', type = 'quantitative', title ='sea ice extent (sq. km)'), color=alt.Color('Month:N', scale=alt.Scale(range=['rebeccapurple', 'dodgerblue', 'tomato'])))
+points = lines.mark_point().transform_filter(selection)
 
-# Generate a plot for September sea ice extent
-chart = alt.Chart(northern_september_extent).encode(
-            x=alt.X(field='Year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='Extent', type='quantitative', title='sea ice extent (sq. km)', scale=alt.Scale(zero=False)),
-            tooltip=[alt.Tooltip(field='Year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='Extent', type='nominal', title='sea ice extent in September (sq. km)')]
-            ).properties(width='container', height=300)
+rule = base.transform_pivot(
+    'Month', value='Extent', groupby=['Year']
+).mark_rule().encode(
+    opacity=alt.condition(selection, alt.value(0.3), alt.value(0)),
+    tooltip=[alt.Tooltip(field = c, type='quantitative') for c in columns]
+).add_selection(selection)
 
-l5 = chart.mark_line(color='dodgerblue')
-p5 = chart.mark_point(color='dodgerblue', filled=True)
-
-# Generate a plot for Annual sea ice extent
-chart = alt.Chart(northern_annual_extent).encode(
-            x=alt.X(field='Year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='Extent', type='quantitative', title='sea ice extent (sq. km)', scale=alt.Scale(zero=False)),
-            tooltip=[alt.Tooltip(field='Year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='Extent', type='nominal', title='annually averaged sea ice extent (sq. km)')]
-            ).properties(width='container', height=300)
-
-l6 = chart.mark_line(color='dodgerblue')
-p6 = chart.mark_point(color='dodgerblue', filled=True)
-
-layer2 = alt.layer(l4,p4,l5,p5,l6,p6)
-layer2.save('./northern_extent.json')
+northern_extent_layer = alt.layer(lines, points, rule)
+northern_extent_layer.save('./northern_extent.json')
--- data/water/NOAA_sea_ice_index/viz/generate_southern.py
+++ data/water/NOAA_sea_ice_index/viz/generate_southern.py
@@ -6,82 +6,49 @@ import anthro.io
 # Load the Southern Hemisphere sea ice area data
 southern_area_data = pd.read_csv('../processed/southern_hemisphere_area.csv')
 southern_area_data['Year'] = pd.to_datetime(southern_area_data['Year'], format='%Y')
-southern_february_area = southern_area_data[southern_area_data['Month']=='February']
-southern_september_area = southern_area_data[southern_area_data['Month']=='September']
-southern_annual_area = southern_area_data[southern_area_data['Month']=='Annual']
+southern_area_data = southern_area_data.drop(southern_area_data[(southern_area_data.Month != 'Annual') & (southern_area_data.Month != 'February') & (southern_area_data.Month != 'September')].index)
 
-# Generate a plot for February sea ice area
-chart = alt.Chart(southern_february_area).encode(
-            x=alt.X(field='Year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='Area', type='quantitative', title='sea ice area (sq. km)', scale=alt.Scale(zero=False)),
-            tooltip=[alt.Tooltip(field='Year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='Area', type='nominal', title='sea ice area in February (sq. km)')]
-            ).properties(width='container', height=300)
-l1 = chart.mark_line(color='dodgerblue')
-p1 = chart.mark_point(color='dodgerblue', filled=True)
+# Generate a plot for sea ice area in February, September, and Annual
+base = alt.Chart(southern_area_data).encode(alt.X(field='Year', type='temporal', timeUnit='year', title='year')).properties(width='container', height=300)
+columns = sorted(southern_area_data.Month.unique())
+selection = alt.selection_single(
+    fields=['Year'], nearest=True, on='mouseover', empty='none', clear='mouseout'
+)
 
-# Generate a plot for September sea ice area
-chart = alt.Chart(southern_september_area).encode(
-            x=alt.X(field='Year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='Area', type='quantitative', title='sea ice area (sq. km)', scale=alt.Scale(zero=False)),
-            tooltip=[alt.Tooltip(field='Year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='Area', type='nominal', title='sea ice area in September (sq. km)')]
-            ).properties(width='container', height=300)
-l2 = chart.mark_line(color='dodgerblue')
-p2 = chart.mark_point(color='dodgerblue', filled=True)
+lines = base.mark_line(point=True).encode(alt.Y(field='Area', type = 'quantitative', title ='sea ice area (sq. km)'), color=alt.Color('Month:N', scale=alt.Scale(range=['rebeccapurple', 'tomato', 'dodgerblue'])))
+points = lines.mark_point().transform_filter(selection)
 
-# Generate a plot for Annual sea ice area
-chart = alt.Chart(southern_annual_area).encode(
-            x=alt.X(field='Year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='Area', type='quantitative', title='sea ice area (sq. km)', scale=alt.Scale(zero=False)),
-            tooltip=[alt.Tooltip(field='Year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='Area', type='nominal', title='annually averaged sea ice area (sq. km)')]
-            ).properties(width='container', height=300)
-l3 = chart.mark_line(color='dodgerblue')
-p3 = chart.mark_point(color='dodgerblue', filled=True)
+rule = base.transform_pivot(
+    'Month', value='Area', groupby=['Year']
+).mark_rule().encode(
+    opacity=alt.condition(selection, alt.value(0.3), alt.value(0)),
+    tooltip=[alt.Tooltip(field = c, type='quantitative') for c in columns]
+).add_selection(selection)
 
-layer = alt.layer(l1,p1,l2,p2,l3,p3)
-layer.save('./southern_area.json')
+southern_area_layer = alt.layer(lines, points, rule)
+southern_area_layer.save('./southern_area.json')
 
 # Load the Southern Hemisphere sea ice extent data
 southern_extent_data = pd.read_csv('../processed/southern_hemisphere_extent.csv')
 southern_extent_data['Year'] = pd.to_datetime(southern_extent_data['Year'], format='%Y')
-southern_february_extent = southern_extent_data[southern_extent_data['Month']=='February']
-southern_september_extent = southern_extent_data[southern_extent_data['Month']=='September']
-southern_annual_extent = southern_extent_data[southern_extent_data['Month']=='Annual']
+southern_extent_data = southern_extent_data.drop(southern_extent_data[(southern_extent_data.Month != 'Annual') & (southern_extent_data.Month != 'February') & (southern_extent_data.Month != 'September')].index)
 
-# Generate a plot for February sea ice extent
-chart = alt.Chart(southern_february_extent).encode(
-            x=alt.X(field='Year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='Extent', type='quantitative', title='sea ice extent (sq. km)', scale=alt.Scale(zero=False)),
-            tooltip=[alt.Tooltip(field='Year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='Extent', type='nominal', title='sea ice extent in February (sq. km)')]
-            ).properties(width='container', height=300)
+# Generate a plot for sea ice extent in February, September, and Annual
+base = alt.Chart(southern_extent_data).encode(alt.X(field='Year', type='temporal', timeUnit='year', title='year')).properties(width='container', height=300)
+columns = sorted(southern_extent_data.Month.unique())
+selection = alt.selection_single(
+    fields=['Year'], nearest=True, on='mouseover', empty='none', clear='mouseout'
+)
 
-l4 = chart.mark_line(color='dodgerblue')
-p4 = chart.mark_point(color='dodgerblue', filled=True)
+lines = base.mark_line(point=True).encode(alt.Y(field='Extent', type = 'quantitative', title ='sea ice extent (sq. km)'), color=alt.Color('Month:N', scale=alt.Scale(range=['rebeccapurple', 'tomato', 'dodgerblue'])))
+points = lines.mark_point().transform_filter(selection)
 
-# Generate a plot for September sea ice extent
-chart = alt.Chart(southern_september_extent).encode(
-            x=alt.X(field='Year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='Extent', type='quantitative', title='sea ice extent (sq. km)', scale=alt.Scale(zero=False)),
-            tooltip=[alt.Tooltip(field='Year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='Extent', type='nominal', title='sea ice extent in September (sq. km)')]
-            ).properties(width='container', height=300)
+rule = base.transform_pivot(
+    'Month', value='Extent', groupby=['Year']
+).mark_rule().encode(
+    opacity=alt.condition(selection, alt.value(0.3), alt.value(0)),
+    tooltip=[alt.Tooltip(field = c, type='quantitative') for c in columns]
+).add_selection(selection)
 
-l5 = chart.mark_line(color='dodgerblue')
-p5 = chart.mark_point(color='dodgerblue', filled=True)
-
-# Generate a plot for Annual sea ice extent
-chart = alt.Chart(southern_annual_extent).encode(
-            x=alt.X(field='Year', type='temporal', timeUnit='year', title='year'),
-            y=alt.Y(field='Extent', type='quantitative', title='sea ice extent (sq. km)', scale=alt.Scale(zero=False)),
-            tooltip=[alt.Tooltip(field='Year', type='temporal', title='year', format='%Y'),
-                     alt.Tooltip(field='Extent', type='nominal', title='annually averaged sea ice extent (sq. km)')]
-            ).properties(width='container', height=300)
-
-l6 = chart.mark_line(color='dodgerblue')
-p6 = chart.mark_point(color='dodgerblue', filled=True)
-
-layer2 = alt.layer(l4,p4,l5,p5,l6,p6)
-layer2.save('./southern_extent.json')
+southern_extent_layer = alt.layer(lines, points, rule)
+southern_extent_layer.save('./southern_extent.json')
